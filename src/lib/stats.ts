e7d0afbcd69b6ac854d02e8286b5693388f3d1e1
Skye <me@skye.vg>
2023-02-22 00:53:54 +0900

    dummy out pageview

--- src/lib/stats.ts
+++ src/lib/stats.ts
@@ -4,24 +4,24 @@ const plausibleUrl =
   'https://plausible.io/api/v1/stats/aggregate?metrics=pageviews&site_id=moddermore.net&period=30d';
 
 export const getPageviews = async (): Promise<number> => {
-  const {
-    results: {
-      pageviews: { value },
-    },
-  } = await fetch(plausibleUrl, {
-    headers: { Authorization: `Bearer ${process.env.PLAUSIBLE_TOKEN}` },
-  }).then(async (r) => {
-    if (!r.ok)
-      throw new Error(
-        `Error fetching ${r.url}: ${r.status} ${
-          r.statusText
-        }\n${await r.text()}`
-      );
+  // const {
+  //   results: {
+  //     pageviews: { value },
+  //   },
+  // } = await fetch(plausibleUrl, {
+  //   headers: { Authorization: `Bearer ${process.env.PLAUSIBLE_TOKEN}` },
+  // }).then(async (r) => {
+  //   if (!r.ok)
+  //     throw new Error(
+  //       `Error fetching ${r.url}: ${r.status} ${
+  //         r.statusText
+  //       }\n${await r.text()}`
+  //     );
 
-    return r.json();
-  });
+  //   return r.json();
+  // });
 
-  return value;
+  return 0;
 };
 
 export const getUsers = async (): Promise<number> => {
